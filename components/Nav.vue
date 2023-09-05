3b75e31d2e736e4c1be6adc69e80827ec8192e31
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-09-05 14:35:59 +0100

    made changes: ''

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,15 +1,44 @@
 <template>
-  <div>
-
-  </div>
+  <b-navbar spaced fixed-top>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <!-- <img width="36px" src="@/static/uacef_logo.jpg" alt=""> -->
+        <span class="title has-text-primary">REXAPP Admin</span>
+      </b-navbar-item>
+    </template>
+    <template #end>
+      <b-navbar-item href="/">
+        Home
+      </b-navbar-item>
+      <b-navbar-dropdown label="Create">
+        <b-navbar-item href="/create/product">
+          Product
+        </b-navbar-item>
+        <b-navbar-item href="/create/category">
+          Category
+        </b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-dropdown label="View">
+        <b-navbar-item href="/view/gallery">
+          Images
+        </b-navbar-item>
+        <b-navbar-item href="/view/clips">
+          Clips
+        </b-navbar-item>
+      </b-navbar-dropdown>
+      <b-navbar-item href="/#" @click.prevent="logout">
+        Logout
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  methods: {
+    async logout() {
+      this.$store.dispatch('logout')
+    }
   }
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- store/index.js
+++ store/index.js
@@ -33,6 +33,7 @@ export const actions = {
   async logout({ commit }) {
     localStorage.removeItem('user'); // Remove the user item from local storage
     commit('RESET_STORE');
+    this.$router.push('/login')
   },
 }
 
